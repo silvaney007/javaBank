fbc3edd5a3e7d8d9bb9bd54aa75af600575b7516
Sara Lopes <sara.lopes@academiadecodigo.org>
2020-08-11 11:06:42 +0100

    add solution to javabank step 7 spring

--- src/main/java/org/academiadecodigo/javabank/services/mock/MockRecipientService.java
+++ src/main/java/org/academiadecodigo/javabank/services/mock/MockRecipientService.java
@@ -1,6 +1,6 @@
 package org.academiadecodigo.javabank.services.mock;
 
-import org.academiadecodigo.javabank.model.Recipient;
+import org.academiadecodigo.javabank.persistence.model.Recipient;
 
 public class MockRecipientService extends AbstractMockService<Recipient> {
 }
