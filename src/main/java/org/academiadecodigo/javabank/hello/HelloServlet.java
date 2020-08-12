366c84e59df80ef76d9651987949ca1d654a9636
Academia de Código_ <academiacodigo@kotlin.local>
2020-08-12 17:05:27 +0100

    add webapp and dependencies, tomcat plugin, servlet handson

---
+++ src/main/java/org/academiadecodigo/javabank/hello/HelloServlet.java
@@ -0,0 +1,21 @@
+package org.academiadecodigo.javabank.hello;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+public class HelloServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        PrintWriter writer = resp.getWriter();
+        writer.println("<html>");
+        writer.println("<body>");
+        writer.println("<h1>HELLO WORLD</h1>");
+        writer.println("</body>");
+        writer.println("</html>");
+    }
+}
--- src/main/java/org/academiadecodigo/javabank/services/mock/MockRecipientService.java
+++
@@ -1,6 +0,0 @@
-package org.academiadecodigo.javabank.services.mock;
-
-import org.academiadecodigo.javabank.persistence.model.Recipient;
-
-public class MockRecipientService extends AbstractMockService<Recipient> {
-}
