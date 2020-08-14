27898ee23afcb36b1be39812e95fb8e344b00470
Mariana <mariana.barros@academiadecodigo.org>
2020-08-14 10:10:03 +0100

    add step-9 our cute and loved Spring MVC

--- src/test/java/org/academiadecodigo/javabank/persistence/jpa/dao/JpaRecepientDaoTest.java
+++ src/test/java/org/academiadecodigo/javabank/persistence/jpa/dao/JpaRecepientDaoTest.java
@@ -17,7 +17,7 @@ import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.*;
 
-public class JpaRecipientDaoTest {
+public class JpaRecepientDaoTest {
 
     private JpaRecipientDao recipientDao;
     private EntityManager em;
